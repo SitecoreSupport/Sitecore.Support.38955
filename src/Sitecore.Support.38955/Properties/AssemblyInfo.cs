77f85854865ddc4481dc179d87cdd3d36095cc9a
DK\mao <mao@sitecore.net>
2018-05-30 18:38:55 +0300

    Initial commit

---
+++ src/Sitecore.Support.38955/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.38955")]
+[assembly: AssemblyProduct("Sitecore.Support.38955")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.38955/Publishing/Service/Client/Commands/Publish.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Framework.Conditions;
+using Sitecore.Globalization;
+using Sitecore.Web.UI.Sheer;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Publishing.Service.Client.Commands
+{
+  public class Publish : Sitecore.Publishing.Service.Client.Commands.Publish
+  {
+    protected void Run(ClientPipelineArgs args)
+    {
+      Condition.Requires<ClientPipelineArgs>(args, "args").IsNotNull<ClientPipelineArgs>();
+      Condition.Requires<NameValueCollection>(args.Parameters, "context.Parameters").IsNotNull<NameValueCollection>();
+      string itemPath = args.Parameters["id"];
+      string name = args.Parameters["language"];
+      string value = args.Parameters["version"];
+      Item item = Context.ContentDatabase.Items[itemPath, Language.Parse(name), Sitecore.Data.Version.Parse(value)];
+      if (item == null)
+      {
+        SheerResponse.Alert("Item not found.", new string[0]);
+        return;
+      }
+      if (!this.IsWorkflowPublishable(args, item))
+      {
+        return;
+      }
+      SheerResponse.Broadcast(SheerResponse.ShowModalDialog(new ModalDialogOptions(string.Format("{0}?id={1}&sourceDatabase={2}", "/sitecore/shell/sitecore/client/Applications/Publishing/PublishDialog", item.ID, item.Database.Name))
+      {
+        Width = "650",
+        Height = "750",
+        ForceDialogSize = true
+      }), "Shell");
+    }
+  }
+}
