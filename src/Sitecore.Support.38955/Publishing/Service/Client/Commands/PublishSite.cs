13e7613fd8203721d0ba060020000387ad1f05ce
DK\mao <mao@sitecore.net>
2018-06-01 16:00:42 +0300

    Check item changes before publishing site or clicking Publish Now button

---
+++ src/Sitecore.Support.38955/Publishing/Service/Client/Commands/PublishSite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Framework.Conditions;
+using Sitecore.Web.UI.Sheer;
+using System.Collections.Specialized;
+
+namespace Sitecore.Support.Publishing.Service.Client.Commands
+{
+  public class PublishSite : Sitecore.Publishing.Service.Client.Commands.PublishSite
+  {
+    public override void Execute(CommandContext context)
+    {
+      Condition.Requires<CommandContext>(context, "context").IsNotNull<CommandContext>();
+      Condition.Requires<NameValueCollection>(context.Parameters, "context.Parameters").IsNotNull<NameValueCollection>();
+      SheerResponse.CheckModified(false);
+      SheerResponse.Broadcast(SheerResponse.ShowModalDialog(new ModalDialogOptions(string.Format("{0}?sourceDatabase={1}", "/sitecore/shell/sitecore/client/Applications/Publishing/PublishDialog", Context.ContentDatabase.Name))
+      {
+        Width = "650",
+        Height = "750",
+        ForceDialogSize = true
+      }), "Shell");
+    }
+  }
+}
