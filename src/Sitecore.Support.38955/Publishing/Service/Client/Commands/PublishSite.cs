f592a00d15f0854212a1cec964dd7b357c04b5d1
DK\KIV <kirill.volovitskiy@sitecore.net>
2019-05-27 13:31:39 +0300

    set ResumePreviousPipeline to true when opening Site publish dialog

--- src/Sitecore.Support.38955/Publishing/Service/Client/Commands/PublishSite.cs
+++ src/Sitecore.Support.38955/Publishing/Service/Client/Commands/PublishSite.cs
@@ -16,7 +16,20 @@ namespace Sitecore.Support.Publishing.Service.Client.Commands
     {
       Condition.Requires<CommandContext>(context, "context").IsNotNull<CommandContext>();
       Condition.Requires<NameValueCollection>(context.Parameters, "context.Parameters").IsNotNull<NameValueCollection>();
-      SheerResponse.CheckModified(false);
+
+      Context.ClientPage.Start(this, "Run", new NameValueCollection());
+    }
+
+    protected virtual void Run(ClientPipelineArgs args)
+    {   
+      if (!SheerResponse.CheckModified(new CheckModifiedParameters
+      {
+        ResumePreviousPipeline = true
+      }))
+      {
+        return;
+      }
+
       SheerResponse.Broadcast(SheerResponse.ShowModalDialog(new ModalDialogOptions(string.Format("{0}?sourceDatabase={1}", "/sitecore/shell/sitecore/client/Applications/Publishing/PublishDialog", Context.ContentDatabase.Name))
       {
         Width = "650",
